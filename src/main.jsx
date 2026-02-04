434edbab51daac054c698716ac9b57129fd7c850
Raja khan <rajak82001@gmail.com>
2026-02-05 04:38:32 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthContext";
+import { HashRouter } from "react-router-dom";
 // import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
