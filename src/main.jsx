d98397ea269a78da61bb5780c8a88cf3a17023db
Raja khan <rajak82001@gmail.com>
2026-02-05 04:56:14 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthContext";
-import { HashRouter } from "react-router-dom";
-// import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <AuthProvider>
         <App />
       </AuthProvider>
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>,
 );
